1b9a3a3078b0a840556b1788a0e84e9121d5925c
syncerpn <nghianteeek57@gmail.com>
2025-09-17 11:30:17 +0900

    Update 2144c.py

--- 2144c.py
+++ 2144c.py
@@ -1,5 +1,17 @@
 t = int(input())
+MOD = 998244353
 for _ in range(t):
     n = int(input())
     A = list(map(int, input().strip().split(" ")))
     B = list(map(int, input().strip().split(" ")))
+    p, q = 0, 0
+    ans = 1
+    for a, b in zip(A, B):
+        if min(a, b) >= max(p, q):
+            ans = (ans * 2) % MOD
+        elif min(a, b) < min(p, q):
+            print(0)
+            break
+        p, q = a, b
+    else:
+        print(ans)
